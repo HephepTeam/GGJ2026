a58ccf103259915196fbaa7e59442c8a98733ff8
Hephep <33314426+HephepTeam@users.noreply.github.com>
2026-02-01 00:36:19 +0100

    super animation de mort de ouf

--- scenes/Player/player.gd
+++ scenes/Player/player.gd
@@ -9,6 +9,7 @@ var points_scene: PackedScene = preload("res://scenes/points.tscn")
 
 @export var projectile_scene: PackedScene
 @export var damage_bomb_scene : PackedScene
+@export var broken_mask_scene: PackedScene
 
 @export var speed := 500.0
 @export var health := 100.0
@@ -21,35 +22,41 @@ var points_scene: PackedScene = preload("res://scenes/points.tscn")
 
 @export var projectile_data : ProjectileData
 
+
+var is_dead := false
+
 var body_direction = 1.0
 var _cooldown_hit = false
 
 var knockback_dir : Vector2
 var knockback_force := 200.0
 
+func _ready() -> void:
+	Globals.player = self
 
 func _physics_process(delta):
-	var direction = Input.get_vector(
-		'ui_left', 'ui_right', 'ui_up', 'ui_down')
+	if !is_dead:
+		var direction = Input.get_vector(
+			'ui_left', 'ui_right', 'ui_up', 'ui_down')
 
-	if direction.length():
-		velocity = velocity.lerp(direction * speed, delta * 12.0)
+		if direction.length():
+			velocity = velocity.lerp(direction * speed, delta * 12.0)
 
-		%Body.play("run")
-		if abs(velocity.x) > 0.01:
-			body_direction = sign(velocity.x)
-			body.scale.x = body_direction
-			%Visuals.skew = lerp(%Visuals.skew, deg_to_rad(body_direction*12.0), delta*80)
+			%Body.play("run")
+			if abs(velocity.x) > 0.01:
+				body_direction = sign(velocity.x)
+				body.scale.x = body_direction
+				%Visuals.skew = lerp(%Visuals.skew, deg_to_rad(body_direction*12.0), delta*80)
 
-	else:
-		%Body.play("idle")
-		%Visuals.skew = lerp(%Visuals.skew, 0.0, delta*80)
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.y = move_toward(velocity.y, 0, speed)
+		else:
+			%Body.play("idle")
+			%Visuals.skew = lerp(%Visuals.skew, 0.0, delta*80)
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.y = move_toward(velocity.y, 0, speed)
 
-	velocity += -knockback_dir
-	knockback_dir = knockback_dir.lerp(Vector2.ZERO, delta * 5.0)
-	move_and_slide()
+		velocity += -knockback_dir
+		knockback_dir = knockback_dir.lerp(Vector2.ZERO, delta * 5.0)
+		move_and_slide()
 
 func _process(delta: float) -> void:
 	if health < MAX_HEALTH:
@@ -77,13 +84,14 @@ func _check_for_nearest_enemy() -> Enemy:
 	return nearest
 
 func shoot(target: Vector2):
-	var inst = projectile_scene.instantiate()
-	var dir = (target - global_position).normalized()
-	inst.direction = dir
-	inst.data = projectile_data
-	Globals.entities_container.add_child(inst)
-	inst.global_position = shoot_point.global_position
-	inst.touched.connect(_on_projectile_touched)
+	if !is_dead:
+		var inst = projectile_scene.instantiate()
+		var dir = (target - global_position).normalized()
+		inst.direction = dir
+		inst.data = projectile_data
+		Globals.entities_container.add_child(inst)
+		inst.global_position = shoot_point.global_position
+		inst.touched.connect(_on_projectile_touched)
 
 
 func add_damage_points(damage: float) -> void:
@@ -95,7 +103,7 @@ func add_damage_points(damage: float) -> void:
 
 
 func get_damage(val: int, dir: Vector2):
-	if !_cooldown_hit:
+	if !_cooldown_hit and !is_dead:
 		_cooldown_hit = true
 		knockback_dir = dir * knockback_force
 		health = clamp(health - val, 0.0, MAX_HEALTH)
@@ -103,6 +111,12 @@ func get_damage(val: int, dir: Vector2):
 		add_damage_points(val)
 		cooldown_hit.start(cooldown_hit_duration)
 		if health <= 0:
+			body.play("idle")
+			is_dead = true
+			var inst = broken_mask_scene.instantiate()
+			add_child(inst)
+			inst.position = shoot_point.position
+			Globals.launch_slowmo()
 			dead.emit()
 
 
