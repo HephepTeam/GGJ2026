075cbd3132b4f9d952b5b82ef456c0e85e1eb8d5
Hephep <33314426+HephepTeam@users.noreply.github.com>
2026-01-31 10:17:36 +0100

    gestion data projectile

--- scenes/Player/projectile.gd
+++ scenes/Player/projectile.gd
@@ -1,11 +1,10 @@
 extends Area2D
 class_name Projectile
 
+var data: ProjectileData
+
 var direction : Vector2
 var speed : float = 200.0
-
-func _ready():
-	pass
 	
 	
 func _physics_process(delta: float) -> void:
