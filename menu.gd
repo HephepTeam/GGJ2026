523b086d3a04adf3e009728df2e1d15a1bff3a6a
babacar <coding@MacBook-Pro-de-Babacar.local>
2026-01-31 04:04:28 +0100

    add beautiful menu v0.0.0.0.1 Alpha

---
+++ menu.gd
@@ -0,0 +1,74 @@
+extends Control
+
+signal maskButton_hover
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+
+
+
+
+
+
+func _on_select_mask_button_pressed() -> void:
+	$Control/VBoxContainer/SelectMaskButton/Panel.visible = true
+	$Control/VBoxContainer/SelectMaskButton/SlideR.start()
+
+func _on_select_mask_button_mouse_entered() -> void:
+	if $Control/VBoxContainer/SelectMaskButton/Panel.visible == false : 
+		maskButton_hover.emit()
+	
+
+
+func _on_return_button_pressed() -> void:
+	$Control/VBoxContainer/SelectMaskButton/Panel.visible = false
+
+
+func _on_mask_2_button_mouse_entered() -> void:
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = true
+		
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = true
+
+
+
+
+func _on_mask_3_button_mouse_entered() -> void:
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = true
+		
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = true
+
+
+func _on_mask_1_button_mouse_entered() -> void:
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = true
+		
+	if $Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$Control/VBoxContainer/SelectMaskButton/Panel/VBoxContainer/HBoxContainer/LabelMask1.visible = true
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
