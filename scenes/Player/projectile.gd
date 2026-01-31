eb0903174b0d6074f46eea1967234163d4da0a4f
Hephep <33314426+HephepTeam@users.noreply.github.com>
2026-01-31 10:55:05 +0100

    delete projectile if out of screen

--- scenes/Player/projectile.gd
+++ scenes/Player/projectile.gd
@@ -10,3 +10,5 @@ var speed : float = 200.0
 func _physics_process(delta: float) -> void:
 	position += direction * speed * delta
 	
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	queue_free()
