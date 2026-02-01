423c27487f085a202dac88b80ec78cd88ae1948b
Bertrand Bordage <bordage.bertrand@gmail.com>
2026-02-01 01:10:16 +0100

    Makes the shaders show their full effect on pickup, then back to their current value.

--- scenes/filters.gd
+++ scenes/filters.gd
@@ -3,6 +3,19 @@ extends ColorRect
 
 var player: Player
 
+var speed := 0.0:
+	set(value):
+		speed = value
+		material.set_shader_parameter("speed", value)
+var strength := 0.0:
+	set(value):
+		strength = value
+		material.set_shader_parameter("strength", value)
+var explosion := 0.0:
+	set(value):
+		explosion = value
+		material.set_shader_parameter("explosion", value)
+
 
 func _ready() -> void:
 	Globals.mask_picked_up.connect(on_mask_picked_up)
@@ -18,6 +31,20 @@ func _process(_delta: float) -> void:
 
 
 func on_mask_picked_up(_mask_data: MaskData) -> void:
-	material.set_shader_parameter("speed", clamp(Globals.speed_multiplier - 1.0, 0.0, 1.0))
-	material.set_shader_parameter("strength", clamp(Globals.strength_multiplier - 1.0, 0.0, 1.0))
-	material.set_shader_parameter("explosion", clamp(Globals.explosion_multiplier - 1.0, 0.0, 1.0))
+	var new_speed := clampf(Globals.speed_multiplier - 1.0, 0.0, 1.0)
+	var new_strength := clampf(Globals.strength_multiplier - 1.0, 0.0, 1.0)
+	var new_explosion := clampf(Globals.explosion_multiplier - 1.0, 0.0, 1.0)
+	if abs(new_speed - speed) > 0.0001:
+		var tween := create_tween()
+		tween.tween_property(self, "speed", 1.0, 0.2)
+		tween.tween_property(self, "speed", new_speed, 0.2)
+
+	if abs(new_strength - strength) > 0.0001:
+		var tween := create_tween()
+		tween.tween_property(self, "strength", 1.0, 0.2)
+		tween.tween_property(self, "strength", new_strength, 0.2)
+
+	if abs(new_explosion - explosion) > 0.0001:
+		var tween := create_tween()
+		tween.tween_property(self, "explosion", 1.0, 0.2)
+		tween.tween_property(self, "explosion", new_explosion, 0.2)
