f8c12dff54fdbde77f15e1425ed824e2c8c434b8
Hephep <33314426+HephepTeam@users.noreply.github.com>
2026-02-01 10:23:03 +0100

    Minimap

--- scenes/minimap.gd
+++ scenes/minimap.gd
@@ -2,5 +2,6 @@ extends Control
 
 
 func _process(delta: float) -> void:
-	$pos.position = Globals.player.position/Vector2(1024,1024)*Vector2(32,32)
+	$pos.position = (Globals.player.position-Vector2(398+300/2,512))/Vector2(1024,1024)*Vector2(64,64)
+	$pos.position = snapped($pos.position, Vector2(64,64))
 	
