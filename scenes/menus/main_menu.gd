8198f1cf791ddace06bd0d6e4fb0eb8d853fecff
babacar <coding@MacBook-Pro-de-Babacar.local>
2026-02-01 03:16:26 +0100

    FIX: menu and launch good scene

--- scenes/menus/main_menu.gd
+++ scenes/menus/main_menu.gd
@@ -19,60 +19,60 @@ func _process(delta: float) -> void:
 
 
 func _on_select_mask_button_pressed() -> void:
-	$VBoxContainer/SelectMaskButton/Panel.visible = true
-	if $VBoxContainer/Scoring/PanelScoring.visible == true:
-		$VBoxContainer/Scoring/PanelScoring.visible = false;
-	if $VBoxContainer/ConfigButton/PanelConfig.visible == true:
-		$VBoxContainer/ConfigButton/PanelConfig.visible = false;
+	$MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible = true
+	if $MarginContainer/VBoxContainer/Scoring/PanelScoring.visible == true:
+		$MarginContainer/VBoxContainer/Scoring/PanelScoring.visible = false;
+	if $MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible == true:
+		$MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible = false;
 
 
 func _on_select_mask_button_mouse_entered() -> void:
-	if $VBoxContainer/SelectMaskButton/Panel.visible == false : 
-		if $VBoxContainer/SelectMaskButton.position.x <=20.0 :
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible == false : 
+		if $MarginContainer/VBoxContainer/SelectMaskButton.position.x <=20.0 :
 			maskButton_hover.emit()
 	
 
 
 func _on_return_button_pressed() -> void:
-	$VBoxContainer/SelectMaskButton/Panel.visible = false
+	$MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible = false
 
 
 func _on_mask_2_button_mouse_entered() -> void:
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = true
 		
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = true
 
 
 
 
 func _on_mask_3_button_mouse_entered() -> void:
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = true
 		
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = true
 
 
 func _on_mask_1_button_mouse_entered() -> void:
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = true
 		
-	if $VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
-		$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = true
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask3.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask2.visible = false
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/HBoxContainer/LabelMask1.visible = true
 
 
 func _on_quit_button_pressed() -> void:
@@ -81,32 +81,32 @@ func _on_quit_button_pressed() -> void:
 
 
 func _on_select_mask_button_mouse_exited() -> void:
-		if $VBoxContainer/SelectMaskButton/Panel.visible == false : 
-			if $VBoxContainer/SelectMaskButton.position.x >= 20.0 :
-				$VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/ReturnButton/SlideL.start()
+		if $MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible == false : 
+			if $MarginContainer/VBoxContainer/SelectMaskButton.position.x >= 20.0 :
+				$MarginContainer/VBoxContainer/SelectMaskButton/Panel/MarginContainer/VBoxContainer/ReturnButton/SlideL.start()
 
 
 func _on_start_game_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/game_scene.tscn")
+	get_tree().change_scene_to_file("res://scenes/MasterGameScene.tscn")
 	
 
 
 func _on_return_scoring_button_pressed() -> void:
-	$VBoxContainer/Scoring/PanelScoring.visible = false;
+	$MarginContainer/VBoxContainer/Scoring/PanelScoring.visible = false;
 
 
 func _on_scoring_pressed() -> void:
-	$VBoxContainer/Scoring/PanelScoring.visible = true;
-	if $VBoxContainer/ConfigButton/PanelConfig.visible == true:
-		$VBoxContainer/ConfigButton/PanelConfig.visible = false;
-	if $VBoxContainer/SelectMaskButton/Panel.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel.visible = false;
+	$MarginContainer/VBoxContainer/Scoring/PanelScoring.visible = true;
+	if $MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible == true:
+		$MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible = false;
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible = false;
 func _on_config_button_pressed() -> void:
-	$VBoxContainer/ConfigButton/PanelConfig.visible = true;
-	if $VBoxContainer/Scoring/PanelScoring.visible == true:
-		$VBoxContainer/Scoring/PanelScoring.visible = false;
-	if $VBoxContainer/SelectMaskButton/Panel.visible == true:
-		$VBoxContainer/SelectMaskButton/Panel.visible = false;
+	$MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible = true;
+	if $MarginContainer/VBoxContainer/Scoring/PanelScoring.visible == true:
+		$MarginContainer/VBoxContainer/Scoring/PanelScoring.visible = false;
+	if $MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible == true:
+		$MarginContainer/VBoxContainer/SelectMaskButton/Panel.visible = false;
 
 
 func _on_vol_princ_h_slider_value_changed(value: float) -> void:
@@ -117,14 +117,15 @@ func _on_vol_princ_h_slider_value_changed(value: float) -> void:
 
 
 func _on_return_config_button_pressed() -> void:
-	$VBoxContainer/ConfigButton/PanelConfig.visible = false
+	$MarginContainer/VBoxContainer/ConfigButton/PanelConfig.visible = false
 
 
 func _input(event):
 	if event.is_action_pressed("ui_accept") and $TitleLBL.visible: # "ui_cancel" est la touche Echap par défaut
 		get_tree().get_root().set_input_as_handled()
-		$VBoxContainer.visible = true
+		$MarginContainer/VBoxContainer.visible = true
 		$PressALBL.visible = false
 		$TitleLBL.visible = false
 		%StartGameButton.grab_focus()
+		$Title2LBL.visible = true
 			
